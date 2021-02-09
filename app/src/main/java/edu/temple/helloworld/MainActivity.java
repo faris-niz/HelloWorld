ea83d8629f9ba3eac5da379480da4aa4b36d42f4
Faris Awad <tuk86431@temple.edu>
2021-02-09 15:42:58 -0500

    update textView with button click

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -6,19 +6,24 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button myButton;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button myButton = findViewById(R.id.mybutton);
+        myButton = findViewById(R.id.mybutton);
+        textView = findViewById(R.id.textView);
+
+
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You have clicked the button", Toast.LENGTH_SHORT).show();
+                textView.setText("You have clicked the button");
             }
         });
 
